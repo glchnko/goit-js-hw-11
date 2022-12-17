69b1258252d73a15c213f19a70939b265856bd28
Anna <agalucnko@gmail.com>
2022-12-18 00:11:17 +0200

    add HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const axios = require('axios').default;
